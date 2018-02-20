f6f061d6e9534b9c0e04e5f01875b06f3b699f5d
pei jason <peijd@pzlteam.com>
2018-02-20 22:35:04 +0900

    添加。

---
+++ src/main/java/spring/beans/xml/XMLConfig.java
@@ -0,0 +1,41 @@
+package spring.beans.xml;
+
+/**
+ * (       "     )
+ * ( _  *
+ * * (     /      \    ___
+ * "     "        _/ /
+ * (   *  )    ___/   |
+ * )   "     _ o)'-./__
+ * *  _ )    (_, . $$$
+ * (  )   __ __ 7_ $$$$
+ * ( :  { _)  '---  $\
+ * ______'___//__\   ____, \
+ * )           ( \_/ _____\_
+ * .'             \   \------''.
+ * |='           '=|  |         )
+ * |               |  |  .    _/
+ * \    (. ) ,   /  /__I_____\
+ * snd  '._/_)_(\__.'   (__,(__,_]
+ * <o>
+ * |   愿你被这个世界温柔以待。
+ * / \
+ *
+ * @author peijundong@gmail.com
+ * Created by peijundong on 2018/2/20。
+ * Description:
+ * Copyright(©) 2018 by peijundong
+ */
+public class XMLConfig {
+
+    private  String xmlPath;
+
+
+    public String getXmlPath() {
+        return xmlPath;
+    }
+
+    public void setXmlPath(String xmlPath) {
+        this.xmlPath = xmlPath;
+    }
+}
