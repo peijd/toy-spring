1769004b5db4e5e195f43c39755d43b8cea52978
pei jason <peijd@pzlteam.com>
2018-02-21 22:25:42 +0900

    添加。

---
+++ src/test/java/spring/beans/xml/TestClojureGenerateService.java
@@ -0,0 +1,49 @@
+package spring.beans.xml;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * (       "     )
+ * ( _  *
+ * * (     /      \    ___
+ * "     "        _/ /
+ * (   *  )    ___/   |
+ * )   "     _ o)'-./__
+ * *  _ )    (_, . $$$
+ * (  )   __ __ 7_ $$$$
+ * ( :  { _)  '---  $\
+ * ______'___//__\   ____, \
+ * )           ( \_/ _____\_
+ * .'             \   \------''.
+ * |='           '=|  |         )
+ * |               |  |  .    _/
+ * \    (. ) ,   /  /__I_____\
+ * snd  '._/_)_(\__.'   (__,(__,_]
+ * <o>
+ * |   愿你被这个世界温柔以待。
+ * / \
+ *
+ * @author peijundong@gmail.com
+ * Created by peijundong on 2018/2/21。
+ * Description:
+ * Copyright(©) 2018 by peijundong
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring-xml-config.xml")
+public class TestClojureGenerateService {
+
+
+    @Autowired
+    private  ClojureGenerateService clojureGenerateService;
+
+    @Test
+    public void testClojure(){
+        Assert.assertEquals("Clojure code", clojureGenerateService.generateCode());
+    }
+
+}
